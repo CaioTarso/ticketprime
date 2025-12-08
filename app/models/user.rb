7d720d79be22e0f025b3b0f438d42886eeb20347
caiotarso <caiotarso7@gmail.com>
2025-12-07 22:30:58 -0300

    feat: criando model "cart" e "cartitem" para lógica de compras de ingressos - Associação com User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one :cart, dependent: :destroy
+  after_create :create_cart
   has_many :events, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
