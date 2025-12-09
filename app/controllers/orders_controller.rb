2af5cc6b6d1e3eee085d23abaaeaf8ba16900787
caiotarso <caiotarso7@gmail.com>
2025-12-09 15:18:25 -0300

    fix: alterando controller para retornar as compras mais recentes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = current_user.orders.includes(:order_items)
+    @orders = current_user.orders.order(created_at: :desc).includes(:order_items)
   end
 
   def show
